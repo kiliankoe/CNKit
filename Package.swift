b578962f0a2aee5fce0782420d3cd8945516f3bd
Kilian Koeltzsch <me@kilian.io>
2017-06-08 22:56:18 +0200

    remove leftover mentions of marshal

--- Package.swift
+++ Package.swift
@@ -1,8 +1,5 @@
 import PackageDescription
 
 let package = Package(
-    name: "CNKit",
-    dependencies: [
-        .Package(url: "https://github.com/utahiosmac/Marshal", majorVersion: 1)
-    ]
+    name: "CNKit"
 )
