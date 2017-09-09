8c3d0002b810c08f4043dfb304a9af1eb3f459e2
Kilian Koeltzsch <me@kilian.io>
2017-09-09 14:47:51 +0200

    update package manifest and overwrite default test

--- Tests/CNKitTests/CNKitTests.swift
+++ Tests/CNKitTests/CNKitTests.swift
@@ -1,22 +1,15 @@
-//
-//  CNKitTests.swift
-//  CNKit
-//
-//  Created by Kilian Koeltzsch on {TODAY}.
-//  Copyright © 2017 CNKit. All rights reserved.
-//
-
-import Foundation
 import XCTest
-import CNKit
+@testable import CNKit
 
 class CNKitTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        //// XCTAssertEqual(CNKit().text, "Hello, World!")
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(CNKit().text, "Hello, World!")
     }
-    
+
+
     static var allTests = [
         ("testExample", testExample),
     ]
