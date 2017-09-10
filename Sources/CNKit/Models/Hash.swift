b00d17cbbbfb025851487fc0c43bef5075fb65bf
Kilian Koeltzsch <me@kilian.io>
2017-09-10 17:57:51 +0200

    add public func to fetch hash values
    ignores the unecessary internal cruft

--- Sources/CNKit/Models/Hash.swift
+++ Sources/CNKit/Models/Hash.swift
@@ -15,15 +15,21 @@ public struct Hash: Codable {
 
 extension Hash: APIResource {
     typealias CollectionType = Hash
+
     static var expectedEncoding = String.Encoding.utf8
 
-    // RequestConfig serves no purpose here, but some kind of type has to be specified for every APIResource. At least it's uninitializable and defaults to nil.
-    struct RequestConfig {
-        private init() {}
-    }
+    // RequestConfig serves no purpose here, but some kind of type has to be specified for every APIResource.
+    struct RequestConfig { }
     typealias RequestResource = RequestConfig?
+
     static func request(to resource: RequestConfig? = nil) -> URLRequest {
         let url = URL(string: "m/json_gebaeude/hash", relativeTo: Config.baseURL)!
         return URLRequest(url: url)
+    }
+
+    /// Fetch the current data hash value.
+    public static func fetch(session: URLSession = URLSession.shared,
+                             completion: @escaping (Result<Hash>) -> Void) {
+        return Hash.fetch(to: nil, session: session, completion: completion)
     }
 }
