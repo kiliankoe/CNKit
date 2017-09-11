75c53fca9c8659a92634ab610f9d3a9bf05886ad
Kilian Koeltzsch <me@kilian.io>
2017-09-11 14:49:34 +0200

    remove unecessary filler stuff from Hash

--- Sources/CNKit/Models/Hash.swift
+++ Sources/CNKit/Models/Hash.swift
@@ -10,11 +10,8 @@ extension Hash: APIResource {
 
     static var expectedEncoding = String.Encoding.utf8
 
-    // RequestConfig serves no purpose here, but some kind of type has to be specified for every APIResource.
-    struct RequestConfig { }
-    typealias RequestResource = RequestConfig?
-
-    static func request(to resource: RequestConfig? = nil) -> URLRequest {
+    struct RequestResource { }
+    static func request(to resource: RequestResource) -> URLRequest {
         let url = URL(string: "m/json_gebaeude/hash", relativeTo: Config.baseURL)!
         return URLRequest(url: url)
     }
@@ -24,9 +21,8 @@ extension Hash: APIResource {
     /// - Parameters:
     ///   - session: session to use, defaults to `.shared`
     ///   - completion: handler
-    /// - Throws: possible error on constructing the request (shouldn't happen in this case)
     public static func fetch(session: URLSession = .shared,
                              completion: @escaping (Result<Hash>) -> Void) {
-        Hash.fetch(resource: nil, body: nil, session: session, completion: completion)
+        Hash.fetch(resource: RequestResource(), body: nil, session: session, completion: completion)
     }
 }
