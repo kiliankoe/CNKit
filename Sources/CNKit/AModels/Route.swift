7c49ecbcd13a20b427f4f4e2400288a1204cf228
Kilian Koeltzsch <me@kilian.io>
2017-09-16 00:39:09 +0200

    Implement Route fetching

---
+++ Sources/CNKit/AModels/Route.swift
@@ -0,0 +1,119 @@
+import Foundation
+import struct CoreLocation.CLLocationCoordinate2D
+
+public struct Route: Decodable {
+    public let length: Double
+    public let duration: Double
+    public let coords: [CLLocationCoordinate2D]
+    public let instructions: [Instruction]
+
+    private enum CodingKeys: String, CodingKey {
+        case length = "route_length"
+        case duration = "route_time"
+        case instructions
+        case coords
+    }
+
+    private enum InstructionsCodingKeys: String, CodingKey {
+        case distances
+        case indications
+        case durations = "mins"
+        case descriptions
+    }
+
+    public init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+
+        self.length = try container.decode(Double.self, forKey: .length)
+        self.duration = try container.decode(Double.self, forKey: .duration)
+
+        let rawCoords = try container.decode([Double].self, forKey: .coords)
+        guard rawCoords.count % 2 == 0 else {
+            throw DecodingError.dataCorrupted(DecodingError.Context(codingPath: [CodingKeys.coords], debugDescription: "odd number of coords received, has to be even"))
+        }
+        let lats = rawCoords.enumerated().filter { $0.offset % 2 == 0 }.map { $1 }
+        let lngs = rawCoords.enumerated().filter { $0.offset % 2 != 0 }.map { $1 }
+        self.coords = zip(lats, lngs).map { CLLocationCoordinate2D(latitude: $0.0, longitude: $0.1) }
+
+        let instructionsContainer = try container.nestedContainer(keyedBy: InstructionsCodingKeys.self, forKey: .instructions)
+
+        let distances = try instructionsContainer.decode([Double].self, forKey: .distances)
+        let indications = try instructionsContainer.decode([Int].self, forKey: .indications)
+        let durations = try instructionsContainer.decode([Int].self, forKey: .durations)
+        let descriptions = try instructionsContainer.decode([String].self, forKey: .descriptions)
+
+        let instructions = zip(zip(distances, indications), zip(durations, descriptions)).map { arg -> Instruction in
+            let (arg0, arg1) = arg
+            let (distance, rawIndication) = arg0
+            let (duration, description) = arg1
+
+            let indication = Instruction.Indication(rawValue: rawIndication)!
+            return Instruction(distance: distance, indication: indication, duration: duration, description: description)
+        }
+
+        self.instructions = instructions
+    }
+}
+
+extension Route {
+    public struct Instruction {
+        public let distance: Double
+        public let indication: Indication
+        public let duration: Int
+        public let description: String
+
+        public enum Indication: Int {
+            case sharpLeft = -3
+            case left = -2
+            case lightLeft = -1
+
+            case `continue` = 0
+
+            case lightRight = 1
+            case right = 2
+            case sharpRight = 3
+
+            case intermediate = 5
+            case roundabout = 6
+
+            case destinationReached = 4
+        }
+    }
+}
+
+extension Route {
+    public enum Mode: String {
+        case foot
+        case bike
+        case wheelchair
+        case car
+    }
+}
+
+extension Route: APIResource {
+    typealias CollectionType = Route
+
+    static var expectedEncoding: String.Encoding = .utf8
+
+    struct RequestResource {
+        let origin: CLLocationCoordinate2D
+        let destination: CLLocationCoordinate2D
+        let mode: Route.Mode
+    }
+
+    static func request(to resource: Route.RequestResource) throws -> URLRequest {
+        let (olat, olng) = (resource.origin.latitude, resource.origin.longitude)
+        let (dlat, dlng) = (resource.destination.latitude, resource.destination.longitude)
+        let url = URL(string: "routingservice/\(olat),\(olng)/\(dlat),\(dlng)/\(resource.mode.rawValue)/geocoordinates", relativeTo: Config.baseURL)!
+        return URLRequest(url: url)
+    }
+
+    public static func fetch(from origin: CLLocationCoordinate2D,
+                             to destination: CLLocationCoordinate2D,
+                             using mode: Route.Mode,
+                             session: URLSession = .shared,
+                             completion: @escaping (Result<Route>) -> Void) {
+        let resource = RequestResource(origin: origin, destination: destination, mode: mode)
+        Route.fetch(resource: resource, session: session, completion: completion)
+    }
+}
---
+++ Tests/CNKitTests/RouteTests.swift
@@ -0,0 +1,66 @@
+import XCTest
+import CNKit
+import struct CoreLocation.CLLocationCoordinate2D
+
+class RouteTests: XCTestCase {
+    func testDecoding() {
+        let json = """
+        {
+          "route_length": 643.073,
+          "instructions": {
+            "distances": [
+              6.443, 8.169, 2.34, 4.026, 2.533, 9.802, 4.63, 4.732, 0.39900000000006, 5.129, 9.697, 7.178, 7.995, 0  ],
+            "indications": [
+              0, 2, -1, -2, 2, -2, 2, -2, -1, 2, -2, 2, -2, 4
+            ],
+            "mins": [
+              0, 0, 0, 0, 0, 0, 0, 1, 2, 0, 0, 0, 0, 0
+            ],
+            "descriptions": [
+              "geradeaus", "rechts abbiegen", "leicht links abbiegen", "links abbiegen", "rechts abbiegen", "links abbiegen", "rechts abbiegen auf Nöthnitzer Straße", "links abbiegen auf Helmholtzstraße", "leicht links abbiegen auf Helmholtzstraße", "rechts abbiegen", "links abbiegen", "rechts abbiegen", "links abbiegen", "Ziel erreicht!"
+            ]
+          },
+          "route_time": 7.7165333333333335,
+          "coords": [
+            13.723288596942021, 51.02547875103679, 13.723312997594022, 51.02553463039252, 13.723399424330877, 51.025517494056764, 13.723424942569991, 51.02551376876638, 13.723595374604956, 51.02554729637982, 13.723866389480227, 51.02560038176776, 13.723928415565084, 51.02572033611805, 13.724515893858282, 51.025621429658415, 13.724554450613732, 51.0257061800146, 13.724904069116057, 51.02564415392975, 13.725308263122475, 51.02557747123191, 13.725434177937377, 51.026235543777844, 13.72545187306669, 51.02633407770844, 13.72521960121139, 51.02715997458607, 13.724981182626959, 51.02816990080856, 13.724973359517158, 51.02820193830584, 13.725510732654723, 51.028261542951945, 13.72560889405628, 51.02827234629405, 13.725467146757255, 51.02877283905684, 13.725430638911515, 51.02888906811675, 13.725823470782268, 51.02890843962673, 13.726245732447037, 51.02892948751739, 13.726233997782334, 51.02900101309272
+          ]
+        }
+        """.data(using: .utf8)!
+
+        let route = try! JSONDecoder().decode(Route.self, from: json)
+
+        XCTAssertEqual(route.length, 643, accuracy: 1)
+        XCTAssertEqual(route.duration, 7, accuracy: 1)
+        XCTAssertEqual(route.coords.count, 23)
+        XCTAssertEqual(route.instructions.count, 14)
+
+        XCTAssertEqual(route.instructions[0].duration, 0)
+        XCTAssertEqual(route.instructions[0].description, "geradeaus")
+        XCTAssertEqual(route.instructions[0].distance, 6, accuracy: 1)
+        XCTAssertEqual(route.instructions[0].indication, .continue)
+    }
+
+    func testFetch() {
+        let origin = CLLocationCoordinate2D(latitude: 13.7232886, longitude: 51.0254788)
+        let destination = CLLocationCoordinate2D(latitude: 13.7262341, longitude: 51.0290011)
+
+        let e = expectation(description: "get data")
+
+        Route.fetch(from: origin, to: destination, using: .foot) { result in
+            guard let route = result.success else {
+                XCTFail("got error: \(result)")
+                e.fulfill()
+                return
+            }
+
+            XCTAssertEqual(route.length, 643, accuracy: 1)
+            XCTAssertEqual(route.duration, 7, accuracy: 1)
+            XCTAssertEqual(route.coords.count, 23)
+            XCTAssertEqual(route.instructions.count, 14)
+
+            e.fulfill()
+        }
+
+        waitForExpectations(timeout: 5)
+    }
+}
