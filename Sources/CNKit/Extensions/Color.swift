bdf881f267fb4d7ade0be83da19af757638427c3
Kilian Koeltzsch <me@kilian.io>
2017-09-21 23:15:30 +0200

    Color documentation

--- Sources/CNKit/Extensions/Color.swift
+++ Sources/CNKit/Extensions/Color.swift
@@ -6,6 +6,9 @@ import AppKit
 
 #if os(iOS)
     public extension UIColor {
+        /// Initialize a new UIColor from a given hex value.
+        ///
+        /// - Parameter rgb: RGB value, easily provided as hex, e.g. 0xAABBCC
         convenience init(withRGB rgb: Int) {
             self.init(red: CGFloat(((rgb & 0xff0000) >> 16))/255.0,
                       green: CGFloat(((rgb & 0xff00) >> 8))/255.0,
@@ -15,6 +18,9 @@ import AppKit
     }
 #elseif os(macOS)
     public extension NSColor {
+        /// Initialize a new NSColor from a given hex value.
+        ///
+        /// - Parameter rgb: RGB value, easily provided as hex, e.g. 0xAABBCC
         convenience init(withRGB rgb: Int) {
             self.init(red: CGFloat(((rgb & 0xff0000) >> 16))/255.0,
                       green: CGFloat(((rgb & 0xff00) >> 8))/255.0,
@@ -26,10 +32,12 @@ import AppKit
 
 public extension Floor.Room {
     #if os(iOS)
+        /// The color this room should be displayed as.
         public var color: UIColor? {
             return UIColor(withRGB: self.rawColor)
         }
     #elseif os(macOS)
+        /// The color this room should be displayed as.
         public var color: NSColor? {
             return NSColor(withRGB: self.rawColor)
         }
