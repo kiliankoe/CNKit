6940766e11d48f29366a7ac78f2dec86001e89c5
Kilian Koeltzsch <me@kilian.io>
2018-01-31 15:02:48 +0100

    add charset to content-type header for req bodies

--- Sources/CNKit/Extensions/URLRequest.swift
+++ Sources/CNKit/Extensions/URLRequest.swift
@@ -4,7 +4,7 @@ internal extension URLRequest {
     mutating func setBody(_ body: [String: Any]) {
         assert(self.httpMethod != "GET", "GET requests shouldn't have a body")
 
-        self.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        self.setValue("application/x-www-form-urlencoded; charset=utf-8", forHTTPHeaderField: "Content-Type")
         self.httpBody = body.asRequestBodyData
     }
 }
